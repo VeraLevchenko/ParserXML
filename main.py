f668b17f4057f81ecbcc89a21cefc706673d5a39
18098 <180982@mail.ru>
2022-05-24 00:47:50 +0700

    mid Good

--- main.py
+++ main.py
@@ -13,7 +13,7 @@ def get_file_list(path):
     return filelist
 
 
-# MIF        Функция парсит кпт и вытаскивает количество контуров, количество точек и их координаты
+#  MIF Функция парсит кпт и вытаскивает количество контуров, количество точек и их координаты
 def make_list_for_mif_actual_land(file_name):
     list_land = []
     tree = ET.parse(file_name)
@@ -38,7 +38,7 @@ def make_list_for_mif_actual_land(file_name):
     return list_land
 
 
-# MIF Функция печатает в файл mif заголовочные данные
+#  MIF Функция печатает в файл mif заголовочные данные
 def print_head_mif():
     file_mif_head = open('actual_land.mif', 'a')
     head_data = [
@@ -64,89 +64,67 @@ def print_head_mif():
 
 
 def make_list_for_mid_actual_land(file_name):
-    list_semantic_land = []
-    list_type_land_record = []
-    list_cad_number = []
-    list_readable_address = []
-    list_permitted_use = []
-    list_area = []
-    list_cost = []
-    list_category = []
-    list_request = []
     tree = ET.parse(file_name)
     root = tree.getroot()
-
+    request = root[1][0].text
+    list_semantic_land = ''
     data = tree.findall('cadastral_blocks/cadastral_block/record_data/base_data/land_records/land_record')
     for data1 in data:
         data2 = data1.findall('./object/common_data/type/value')
-        for type in data2:
-            _type = type.text
-            print(_type)
+        for tipe in data2:
+            _type = tipe.text
         data3 = data1.findall('./object/common_data/cad_number')
         for cad_number in data3:
-            print(cad_number.text)
+            _cad_number = cad_number.text
         data4 = data1.findall('./address_location/address/readable_address')
         for adress in data4:
-            print(adress.text)
+            _adress = adress.text
         data5 = data1.findall('params/permitted_use/permitted_use_established/by_document')
         for permitted_use in data5:
-            print(permitted_use.text)
+            _permitted_use = permitted_use.text
         data6 = data1.findall('params/area/value')
         for area in data6:
-            print(area.text)
+            _area = area.text
         date6 = data1.findall('./cost/value')
         if len(date6) >= 1:
             for cost in date6:
                 _cost = cost.text
         else:
             _cost = "None"
-        print(_cost)
         date7 = data1.findall('params/category/type/value')
         for category in date7:
-            print(category.text)
-        request = root[1][0].text
-        print(request)
-
-
-
-    # file_mid = open('actual_land.mid', 'a')
-    # i = 0
-    # while i < len(list_type_land_record):
-    #     a = ("\"" + list_type_land_record[i] + "\","
-    #          + "\"" + list_cad_number[i] + "\","
-    #          + "\"" + list_readable_address[i] + "\","
-    #          + "\"" + list_permitted_use[i] + "\","
-    #          + "\"" + list_area[i] + "\","
-    #          + "\"" + list_cost[i] + "\","
-    #          + "\"" + list_category[i] + "\","
-    #          + "\"" + list_request[i] + "\"")
-    #     file_mid.write(a + '\n')
-    #     i += 1
-    #     print(i)
-    # file_mid.close()
-    # print(len(list_type_land_record))
-    # print(len(list_cad_number))
-    # print(len(list_readable_address))
-    # print(len(list_permitted_use))
-    # print(len(list_area))
-    # print(len(list_cost))
-    # print(len(list_category))
-    # print(len(list_request))
-
+            _category = category.text
+        # Формирует строку mid файла
+        a = ("\"" + _type +
+             "\"," + "\"" + _cad_number +
+             "\"," + "\"" + _adress +
+             "\"," + "\"" + _permitted_use +
+             "\"," + "\"" + _area +
+             "\"," + "\"" + _cost +
+             "\"," + "\"" + _category +
+             "\"," + "\"" + request +
+             "\"," + "\n")
+        # Формирует данные по всем строкам для записи в mid файл
+        list_semantic_land = (list_semantic_land + a)
     return list_semantic_land
 
 
 if __name__ == '__main__':
-    # print_head_mif()
+     # Запись заголовков в mif
+     print_head_mif()
     # filelist = get_file_list("C:/Users/Necvetaeva_v/PycharmProjects/ParserXML/materials/23.05.2022_12_32_выгрузка/Новая папка")
     filelist = get_file_list("D:\project_Python\ParserXML\materials\_Level1")
     # Открываем поочередно кпт.xml файлы
     for file_name in filelist:
-        # file_mif = open('actual_land.mif', 'a')
-        # list_coordinate_land_record = make_list_for_mif_actual_land(file_name)
-        # # дозаписываем полученный список в файл
-        # for data8 in list_coordinate_land_record:
-        #    file_mif.write(str(data8) + '\n')
-        # file_mif.close()
-        make_list_for_mid_actual_land(file_name)
+        file_mif = open('actual_land.mif', 'a')
+        list_coordinate_land_record = make_list_for_mif_actual_land(file_name)
+        # записываем полученный список в mif файл
+        for data8 in list_coordinate_land_record:
+           file_mif.write(str(data8) + '\n')
+        file_mif.close()
+        # записываем данные mid в файл
+        land = make_list_for_mid_actual_land(file_name)
+        file_mid = open('actual_land.mid', 'a')
+        file_mid.write(land)
+        file_mid.close()
 
